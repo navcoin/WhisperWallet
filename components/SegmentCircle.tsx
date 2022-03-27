f160b071d3257a3fa24c117ebc666d9650c75f26
alex <alex@nav.community>
2022-03-27 19:19:39 +0200

    update spacing and width

--- components/SegmentCircle.tsx
+++ components/SegmentCircle.tsx
@@ -24,7 +24,7 @@ const SegmentCircle = ({
   }, [segmentsSource]);
 
   /* Arcs Calculation*/
-  const initialRotation = 210;
+  const initialRotation = 180;
   const totalArcs = segments.filter((el: any) => el.size > 0).length;
   const totalSpacing = totalArcs > 1 ? totalArcs * minArcSpacing : 0;
   const totalSegmentSize = segments
@@ -80,7 +80,7 @@ const SegmentCircle = ({
             index={index}
             tintColor={segment.color || theme['color-patrick-blue-400']}
             size={svgWidth}
-            width={arcWidth}
+            width={6}
             lineCap={'round'}
             rotation={rotation}
             fill={(arcSweepAngle / 360) * 100}
@@ -135,8 +135,8 @@ SegmentCircle.propTypes = {
 
 SegmentCircle.defaultProps = {
   segmentsSource: [],
-  arcWidth: 10,
-  minArcSpacing: 14,
+  arcWidth: 4,
+  minArcSpacing: 8,
   radius: 100,
   color: '#ADB1CC',
   animationDuration: 1000,
