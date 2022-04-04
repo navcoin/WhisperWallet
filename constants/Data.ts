47873a45f6b9e0ddb3ed942d7bab1d62fd1c7b01
tsejerome <tsejerome1997@gmail.com>
2022-04-04 19:04:35 +0800

    Add server typings

--- constants/Data.ts
+++ constants/Data.ts
@@ -1,4 +1,5 @@
-import {Images} from '../assets/images';
+import { Images } from '../assets/images';
+import { NetworkOption, ServerOption } from './Type';
 
 export const OnBoarding = [
   {
@@ -29,3 +30,54 @@ export const OnBoarding = [
       'Sed eget viverra mi, non placerat risus. Aliquam vel felis eget nunc condimentum hendrerit. Fusce feugiat, nibh suscipit pharetra tempor, risus nulla euismod ante, sit amet rhoncus odio urna ac lorem. Cras elit erat, lacinia vel malesuada vel, cursus eu odio. ',
   },
 ];
+
+export const networkOptions: {
+  [key in NetworkOption]: ServerOption[];
+} = {
+  testnet: [
+    {
+      host: 'electrum-testnet2.nav.community',
+      port: 40004,
+      proto: 'wss',
+      type: 'testnet',
+    },
+    {
+      host: 'electrum-testnet.nav.community',
+      port: 40004,
+      proto: 'wss',
+      type: 'testnet',
+    },
+  ],
+  mainnet: [
+    {
+      host: 'electrum4.nav.community',
+      port: 40004,
+      proto: 'wss',
+      type: 'mainnet',
+    },
+    {
+      host: 'electrum.nextwallet.org',
+      port: 40004,
+      proto: 'wss',
+      type: 'mainnet',
+    },
+    {
+      host: 'electrum2.nav.community',
+      port: 40004,
+      proto: 'wss',
+      type: 'mainnet',
+    },
+    {
+      host: 'electrum3.nav.community',
+      port: 40004,
+      proto: 'wss',
+      type: 'mainnet',
+    },
+    {
+      host: 'electrum.nav.community',
+      port: 40004,
+      proto: 'wss',
+      type: 'mainnet',
+    },
+  ],
+};
