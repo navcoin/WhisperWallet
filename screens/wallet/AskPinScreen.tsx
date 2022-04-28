862775954fff2880b69ecc940617a65e72d5a09f
alex <alex@nav.community>
2022-04-28 11:25:18 +0200

    add info screen before entering pin code

--- screens/wallet/AskPinScreen.tsx
+++ screens/wallet/AskPinScreen.tsx
@@ -1,35 +1,76 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Container from '../../components/Container';
 import {ScreenProps} from '../../navigation/type';
 import {gestureHandlerRootHOC} from 'react-native-gesture-handler';
 import useSecurity from '../../hooks/useSecurity';
 import PINCode from '@haskkor/react-native-pincode';
 import {useNavigation} from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import {moderateScale, scale, verticalScale} from 'react-native-size-matters';
+import {Button, StyleService, Text} from '@tsejerome/ui-kitten-components';
 
 const AskPinScreen: React.FC<ScreenProps<'AskPinScreen'>> = (props: any) => {
   const {setManualPin, setSetManualPin, askManualPin, setAskManualPin} =
     useSecurity();
   const {goBack} = useNavigation();
+  const [showInstructions, setShowInstructions] = useState(true);
   return (
     <Container>
       {setManualPin ? (
-        <PINCode
-          status={'choose'}
-          passwordLength={props.route.params.pinLength}
-          styleMainContainer={{backgroundColor: '#1F2933'}}
-          stylePinCodeTextTitle={{fontFamily: 'Overpass-Bold'}}
-          stylePinCodeTextSubtitle={{fontFamily: 'Overpass-Bold'}}
-          stylePinCodeDeleteButtonText={{fontFamily: 'Overpass-Bold'}}
-          stylePinCodeTextButtonCircle={{fontFamily: 'Overpass-Bold'}}
-          stylePinCodeButtonCircle={{backgroundColor: '#1F2933'}}
-          colorPassword={'#fff'}
-          subtitleChoose={'to protect the access to your wallet'}
-          storePin={async (pin: string) => {
-            setManualPin(pin);
-            goBack();
-            setSetManualPin(undefined);
-          }}
-        />
+        showInstructions ? (
+          <Container
+            useSafeArea
+            style={{
+              flex: 1,
+              alignItems: 'center',
+              justifyContent: 'center',
+              position: 'absolute',
+              left: 0,
+              top: 0,
+              width: '100%',
+              height: '100%',
+            }}>
+            <Icon
+              name={'warning'}
+              size={scale(32)}
+              color={'#fff'}
+              style={{opacity: 1, marginBottom: scale(32)}}
+            />
+            <Text
+              style={{
+                textAlign: 'center',
+                paddingHorizontal: scale(32),
+              }}>
+              In the next screen you are going to be asked to introduce a new{' '}
+              {props.route.params.pinLength}-digit PIN code. {'\n\n'}This code
+              will be asked anytime: {'\n\n'}- A wallet is opened, deleted or
+              resynced {'\n\n'}- Access to the private keys is required {'\n\n'}
+            </Text>
+            <Button
+              style={[styles.wallet]}
+              status={'primary-whisper'}
+              children={'Continue'}
+              onPress={() => setShowInstructions(false)}></Button>
+          </Container>
+        ) : (
+          <PINCode
+            status={'choose'}
+            passwordLength={props.route.params.pinLength}
+            styleMainContainer={{backgroundColor: '#1F2933'}}
+            stylePinCodeTextTitle={{fontFamily: 'Overpass-Bold'}}
+            stylePinCodeTextSubtitle={{fontFamily: 'Overpass-Bold'}}
+            stylePinCodeDeleteButtonText={{fontFamily: 'Overpass-Bold'}}
+            stylePinCodeTextButtonCircle={{fontFamily: 'Overpass-Bold'}}
+            stylePinCodeButtonCircle={{backgroundColor: '#1F2933'}}
+            colorPassword={'#fff'}
+            subtitleChoose={'to protect the access to your wallet'}
+            storePin={async (pin: string) => {
+              setManualPin(pin);
+              goBack();
+              setSetManualPin(undefined);
+            }}
+          />
+        )
       ) : askManualPin ? (
         <PINCode
           status={'enter'}
@@ -59,3 +100,11 @@ const AskPinScreen: React.FC<ScreenProps<'AskPinScreen'>> = (props: any) => {
 };
 
 export default gestureHandlerRootHOC(AskPinScreen);
+
+const styles = StyleService.create({
+  wallet: {
+    marginTop: verticalScale(8),
+    marginBottom: verticalScale(16),
+    width: moderateScale(250, 0.5),
+  },
+});
