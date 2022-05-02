0de260f9ef1bd9ae47028386d411ba072d02bd99
alex v <alex@encrypt-s.com>
2022-05-02 21:27:00 +0200

    Update BalanceCircle.tsx

--- components/BalanceCircle.tsx
+++ components/BalanceCircle.tsx
@@ -1,17 +1,14 @@
-import SegmentCircle from './SegmentCircle';
 import {View} from 'react-native';
 import React, {memo, useEffect, useState} from 'react';
 import useLayout from '../hooks/useLayout';
 import useWallet from '../hooks/useWallet';
-import {useTheme} from '@tsejerome/ui-kitten-components';
 import {Connection_Stats_Enum} from '../constants/Type';
 import CurrencyText from './CurrencyText';
 import Text from './Text';
-import {verticalScale} from 'react-native-size-matters';
-import BackgroundTimer from 'react-native-background-timer';
+import {scale, verticalScale} from 'react-native-size-matters';
+import {AnimatedSegments} from './AnimatedSegments';
 
 const BalanceCircle = memo(() => {
-  const {height} = useLayout();
   const {
     syncProgress,
     bootstrapProgress,
@@ -19,40 +16,8 @@ const BalanceCircle = memo(() => {
     connected,
     firstSyncCompleted,
   } = useWallet();
-  const theme = useTheme();
-  const [balanceSegments, setBalanceSegments] = useState<
-    {
-      size: number;
-      color?: string;
-    }[]
-  >([
-    {
-      size: 100,
-    },
-  ]);
   const [totalBalance, setTotalBalance] = useState(0);
-  const [rotation, setRotation] = useState(180);
-
-  useEffect(() => {
-    if (
-      connected != Connection_Stats_Enum.Connecting &&
-      connected != Connection_Stats_Enum.Bootstrapping
-    ) {
-      BackgroundTimer.stopBackgroundTimer();
-      setRotation(180);
-    } else {
-      BackgroundTimer.stopBackgroundTimer();
-      BackgroundTimer.runBackgroundTimer(() => {
-        setRotation(prev => {
-          return (prev + 2) % 360;
-        });
-      }, 10);
-    }
-    
-    return () => {
-      BackgroundTimer.stopBackgroundTimer();
-    };
-  }, [connected]);
+  const [segments, setSegments] = useState([0, 0, 0]);
 
   useEffect(() => {
     if (balances?.nav) {
@@ -65,19 +30,10 @@ const BalanceCircle = memo(() => {
           balances.staked.pending) /
           1e8,
       );
-      setBalanceSegments([
-        {
-          size: balances.nav.confirmed + balances.nav.pending,
-          color: theme['color-nav-pink'],
-        },
-        {
-          size: balances.xnav.confirmed + balances.xnav.pending,
-          color: theme['color-xnav'],
-        },
-        {
-          size: balances.staked.confirmed + balances.staked.pending,
-          color: theme['color-staking'],
-        },
+      setSegments([
+        balances.nav.confirmed + balances.nav.pending,
+        balances.xnav.confirmed + balances.xnav.pending,
+        balances.staked.confirmed + balances.staked.pending,
       ]);
     }
   }, [balances]);
@@ -89,48 +45,21 @@ const BalanceCircle = memo(() => {
         alignItems: 'center',
         marginBottom: verticalScale(16),
       }}>
-      <SegmentCircle
-        radius={(height * 0.6) / 2 / 2}
-        initialRotation={
-          connected != Connection_Stats_Enum.Connecting &&
-          connected != Connection_Stats_Enum.Bootstrapping
-            ? 180
-            : rotation
-        }
-        background={
+      <AnimatedSegments
+        strokeWidth={4}
+        width={scale(300)}
+        progressVisible={
           connected == Connection_Stats_Enum.Connecting ||
-          connected == Connection_Stats_Enum.Syncing ||
-          connected == Connection_Stats_Enum.Bootstrapping
-            ? theme['color-basic-800']
-            : undefined
+          connected == Connection_Stats_Enum.Bootstrapping ||
+          connected == Connection_Stats_Enum.Syncing
         }
-        segmentsSource={
+        progress={
           connected == Connection_Stats_Enum.Connecting ||
           connected == Connection_Stats_Enum.Bootstrapping
-            ? [
-                {
-                  size: 5,
-                  color: theme['color-nav-pink'],
-                },
-                {
-                  size: 95,
-                  color: 'transparent',
-                },
-              ]
-            : connected == Connection_Stats_Enum.Syncing
-            ? [
-                {
-                  size: syncProgress,
-                  color: theme['color-nav-pink'],
-                },
-                {
-                  size: 100 - syncProgress,
-                  color: 'transparent',
-                },
-              ]
-            : balanceSegments
+            ? -1
+            : syncProgress / 100
         }
-      />
+        segments={segments}></AnimatedSegments>
       {connected == Connection_Stats_Enum.Connecting ? (
         <Text style={{position: 'absolute', textAlign: 'center'}}>
           Connecting...
