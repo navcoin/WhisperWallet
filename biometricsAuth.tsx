6251f9f99f024e03352d9887831176cb69bfc5ea
tsejerome <tsejerome@hotmail.com>
2022-05-10 14:08:18 +0800

    chore: eslint whole repo

--- biometricsAuth.tsx
+++ biometricsAuth.tsx
@@ -1,15 +1,21 @@
-import { 
-   hasHardwareAsync,
-   isEnrolledAsync,
-   authenticateAsync 
+import {
+  hasHardwareAsync,
+  isEnrolledAsync,
+  authenticateAsync,
 } from 'expo-local-authentication';
 const biometricsAuth = async () => {
-    const compatible = await hasHardwareAsync()
-    if (!compatible) throw 'This device is not compatible for biometric authentication'
-    const enrolled = await isEnrolledAsync()
-    if (!enrolled) throw 'This device doesnt have biometric authentication enabled'
-    const result = await authenticateAsync()
-    if (!result.success) throw `${result.error} - Authentication unsuccessful`
-    return
-}
-export default biometricsAuth
+  const compatible = await hasHardwareAsync();
+  if (!compatible) {
+    throw 'This device is not compatible for biometric authentication';
+  }
+  const enrolled = await isEnrolledAsync();
+  if (!enrolled) {
+    throw 'This device doesnt have biometric authentication enabled';
+  }
+  const result = await authenticateAsync();
+  if (!result.success) {
+    throw `${result.error} - Authentication unsuccessful`;
+  }
+  return;
+};
+export default biometricsAuth;
